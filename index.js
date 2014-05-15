a226fe39b3e41df35dcb41bc47fed22724eb5d85
Nathan Zadoks <nathan@nathan7.eu>
2014-05-15 22:40:28 +0200

    always take the fast path for timeouts, don't wait for the watcher

--- index.js
+++ index.js
@@ -157,31 +157,31 @@ Cache.prototype.__acquireWatch = function(args, pending) { var self = this
     , tmp = self.__tmp(digest)
 
   // let's watch if they finish
-  fs.watch(tmp)
-    .on('change', function() {
-      if (timeout) clearTimeout(timeout)
-      this.close()
-      // something changed! let's go check if our file is in the store now.
-      self.__acquire(args, pending)
-    })
+  var watcher = fs.watch(tmp)
+    .on('change', retry)
     .on('error', function(err) {
-      if (timeout) clearTimeout(timeout)
-      this.close()
       if (err.code !== 'ENOENT') return error(err)
-      // they already finished while we were firing up our watcher. let's have another go at everything.
-      self.__acquire(args, pending)
+      retry()
     })
+
+  function retry() {
+    if (timeout) clearTimeout(timeout)
+    if (!watcher) return
+    watcher.close()
+    watcher = null
+    // they already finished while we were firing up our watcher. let's have another go at everything.
+    self.__acquire(args, pending)
+  }
 
   // if we have a timeout, let's check if things haven't gone stale
   // we'll leave this until the next 100ms so we don't fire this up too quickly, stat calls cost
-  var timeout
-  if (self.timeout) setTimeout(checkStale, 100)
+  var timeout = self.timeout && setTimeout(checkStale, 100)
   function checkStale() {
-    timeout = null
     fs.stat(tmp, function(err, stats) {
-      // disappeared in the meanwhile, the watcher will have caught this
-      if (err && err.code === 'ENOENT') return
+      if (!watcher) return
+      if (err && err.code === 'ENOENT') return retry()
       if (err) return error(err)
+
       var delta = new Date() - stats.mtime
 
       if (delta < self.timeout) {
@@ -194,6 +194,7 @@ Cache.prototype.__acquireWatch = function(args, pending) { var self = this
       fs.unlink(tmp, function(err) {
         if (err && err.code === 'ENOENT') return
         if (err) return error(err)
+        retry()
       })
     })
   }
