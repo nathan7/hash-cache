b6576e974501ea275b211f1b0f28ba612729e969
Nathan Zadoks <nathan@nathan7.eu>
2014-05-15 21:48:26 +0200

    refactor the strategies a bunch

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ Cache.prototype.createReadStream = function(digest) { var self = this
       .once('readable', function() { self.__pending.delete(digest) })
     pending.setMaxListeners(0)
     this.__pending.set(digest, pending)
-    this.__acquireFs(arguments, pending)
+    this.__acquire(arguments, pending)
   }
 
   return pending
@@ -44,7 +44,7 @@ Cache.prototype.createReadStream = function(digest) { var self = this
 Cache.prototype.__store = function(digest) { return Path.join(this.path, 'store', digest) }
 Cache.prototype.__tmp = function(digest) { return Path.join(this.path, 'tmp', digest) }
 
-Cache.prototype.__acquireFs = function(args, pending, paranoid) { var self = this
+Cache.prototype.__acquire = function(args, pending, paranoid) { var self = this
   var digest = args[0]
 
   var input = fs.createReadStream(this.__store(digest))
@@ -58,7 +58,7 @@ Cache.prototype.__acquireFs = function(args, pending, paranoid) { var self = thi
 
   this.__hash(input, digest, function(err) {
     if (err) return pending.emit(err)
-    self.__acquireFs(args, pending, false)
+    self.__acquire(args, pending, false)
   })
 }
 
@@ -93,41 +93,8 @@ Cache.prototype.__acquireFresh = function(args, pending) { var self = this
       })
       .on('error', function(err) {
         if (err.code !== 'EEXIST') return error(err)
-        // someone else has already started fetching this
-        // let's watch if they finish
-        fs.watch(tmp)
-          .on('change', function() {
-            if (timeout) clearTimeout(timeout)
-            this.close()
-            // something changed! let's go check if our file is in the store now.
-            self.__acquireFs(args, pending)
-          })
-          .on('error', function(err) {
-            if (timeout) clearTimeout(timeout)
-            this.close()
-            if (err.code !== 'ENOENT') return error(err)
-            // they already finished while we were firing up our watcher. let's have another go at everything.
-            self.__acquireFs(args, pending)
-          })
-
-        // if we have a timeout, let's check if things haven't gone stale
-        // we'll leave this until the next 100ms so we don't fire this up too quickly, stat calls cost
-        var timeout
-        if (self.timeout) setTimeout(checkStale, 100)
-        function checkStale() {
-          timeout = null
-          fs.stat(tmp, function(err, stats) {
-            // disappeared in the meanwhile, the watcher will have caught this
-            if (err && err.code === 'ENOENT') return
-            if (err) return error(err)
-            var delta = new Date() - stats.mtime
-            if (delta < self.timeout) return
-            fs.unlink(tmp, function(err) {
-              if (err && err.code === 'ENOENT') return
-              if (err) return error(err)
-            })
-          })
-        }
+        // someone else has already started fetching this, we'll wait for them
+        self.__acquireWatch(args, pending)
       })
   }
 
@@ -164,6 +131,48 @@ Cache.prototype.__acquireFresh = function(args, pending) { var self = this
 
   function deliver() {
     fs.createReadStream(store).pipe(pending)
+  }
+
+  function error(err) { return pending.emit('error', err) }
+}
+
+Cache.prototype.__acquireWatch = function(args, pending) { var self = this
+  var digest = args[0]
+    , tmp = self.__tmp(digest)
+
+  // let's watch if they finish
+  fs.watch(tmp)
+    .on('change', function() {
+      if (timeout) clearTimeout(timeout)
+      this.close()
+      // something changed! let's go check if our file is in the store now.
+      self.__acquire(args, pending)
+    })
+    .on('error', function(err) {
+      if (timeout) clearTimeout(timeout)
+      this.close()
+      if (err.code !== 'ENOENT') return error(err)
+      // they already finished while we were firing up our watcher. let's have another go at everything.
+      self.__acquire(args, pending)
+    })
+
+  // if we have a timeout, let's check if things haven't gone stale
+  // we'll leave this until the next 100ms so we don't fire this up too quickly, stat calls cost
+  var timeout
+  if (self.timeout) setTimeout(checkStale, 100)
+  function checkStale() {
+    timeout = null
+    fs.stat(tmp, function(err, stats) {
+      // disappeared in the meanwhile, the watcher will have caught this
+      if (err && err.code === 'ENOENT') return
+      if (err) return error(err)
+      var delta = new Date() - stats.mtime
+      if (delta < self.timeout) return
+      fs.unlink(tmp, function(err) {
+        if (err && err.code === 'ENOENT') return
+        if (err) return error(err)
+      })
+    })
   }
 
   function error(err) { return pending.emit('error', err) }
